80cc801c521c4be404a46b396f4472346f99af97
Damon Marc Rocha II <dmarcr1997@gmail.com>
2025-10-24 12:39:11 -0400

    changed CORS origin port

--- pi/submitter/src/server.ts
+++ pi/submitter/src/server.ts
@@ -39,7 +39,7 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const ALLOW_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:8080";
+const ALLOW_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5500";
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", ALLOW_ORIGIN);
